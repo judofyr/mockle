7915b348593018d5f9517adeaabd76e4f582abaf
Magnus Holm <judofyr@gmail.com>
2013-03-10 10:48:47 +0100

    Add some basic template tests

--- test/template_test.js
+++ test/template_test.js
@@ -1,4 +1,30 @@
-test("Hello", function() {
-  ok(1 == '1', 'Passed');
+Mockle.use(['Template', 'Scope'], function() {
+  var T = Mockle.Template;
+  var S = Mockle.Scope;
+  var scope;
+
+  QUnit.testStart(function() {
+    scope = S.create();
+  });
+
+  test("parses text", function() {
+    var tmpl = T.create('test', 'Hello world!');
+    equal(tmpl.name, 'test', 'Name is accessable');
+    equal(tmpl.source, 'Hello world!', 'Source is accessable');
+    ok(tmpl.ast, 'AST exists');
+  });
+
+  test("renders text", function() {
+    var tmpl = T.create('test', 'Hello world!');
+    var res = tmpl.render(scope);
+    equal(res, 'Hello world!');
+  });
+
+  test("renders variables", function() {
+    var tmpl = T.create('test', 'Hello @name!');
+    scope.push({name: 'Mockle'});
+    var res = tmpl.render(scope);
+    equal(res, 'Hello Mockle!');
+  });
 });
 
