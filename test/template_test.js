e417bffd8bd0373d1bef07d88ddab2c4f603dbbd
Magnus Holm <judofyr@gmail.com>
2013-04-06 12:30:36 +0200

    Fix tests

--- test/template_test.js
+++ test/template_test.js
@@ -11,17 +11,20 @@ Mockle.use(['Template', 'Scope'], function() {
     var tmpl = T.create('test', 'Hello world!');
     equal(tmpl.name, 'test', 'Name is accessable');
     equal(tmpl.source, 'Hello world!', 'Source is accessable');
+    tmpl.parse();
     ok(tmpl.ast, 'AST exists');
   });
 
   test("renders text", function() {
     var tmpl = T.create('test', 'Hello world!');
+    tmpl.parse();
     var res = tmpl.render(scope);
     equal(res, 'Hello world!');
   });
 
   test("renders variables", function() {
     var tmpl = T.create('test', 'Hello @name!');
+    tmpl.parse();
     scope.push({name: 'Mockle'});
     var res = tmpl.render(scope);
     equal(res, 'Hello Mockle!');
